d93a0e0963aa0085048e1659830726246cdf7d7c
Raka Priyo <gugun@email.com>
2019-08-28 08:40:02 +0700

    perubahan logo

--- resources/views/FE/libs/navbar.blade.php
+++ resources/views/FE/libs/navbar.blade.php
@@ -42,7 +42,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container">
         <a class="navbar-brand" href="{{url('/')}}">
-            <img src="{{url('bootstrap/vendor/iheritage.png')}}" width="200" height="85">
+            <img src="{{url('bootstrap/vendor/iheritage_texton_pro.png')}}" width="200" height="85">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>

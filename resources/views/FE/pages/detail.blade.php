a1a6ebaf87a0c38798611888c01e9d5b98234a08
Raka Priyo <gugun@email.com>
2019-10-11 22:55:05 +0700

    FE - perbaikan page detail content

--- resources/views/FE/pages/detail.blade.php
+++ resources/views/FE/pages/detail.blade.php
@@ -1,7 +1,7 @@
 @extends('FE.layout')
 @section('content')
 <!-- Header -->
-<header class="ctn-museum-bg py-5">
+<header class="ctn-museum-bg py-5" style="background-image: url('{{$detail->photo}}')">
     <div class="container h-100">
         <div class="row h-100 align-items-center">
         </div>
@@ -39,10 +39,11 @@
             @endif
         </div>
         <div class="col-md-4">
-            <div class="mapouter"><div class="gmap_canvas"><iframe width="350" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=museum%20kepresidenan&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-wordpress-themes/">best wordpress themes</a></div><style>.mapouter{position:relative;text-align:right;height:250px;width:350px;}.gmap_canvas {overflow:hidden;background:none!important;height:250px;width:350px;}</style></div>
+            <div class="mapouter"><div class="gmap_canvas"><iframe width="350" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q={{$detail->map_area_detail}}&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-wordpress-themes/">best wordpress themes</a></div><style>.mapouter{position:relative;text-align:right;height:250px;width:350px;}.gmap_canvas {overflow:hidden;background:none!important;height:250px;width:350px;}</style></div>
             <div class="form-group mt-3">
                 <h5>{{$detail->name}}</h5>
-                <p>{{$detail->address}}</p>
+                <span class="font-weight-lighter">{{$detail->address}}<br>
+                    {{\App\Model\place_tbl::placeNameLang($detail->place_id)}}</span>
             </div>
             <div class="form-group">
                 <h5>@lang('messages.museum_information')</h5>

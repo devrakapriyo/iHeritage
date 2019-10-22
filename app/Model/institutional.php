d6f58b355c3e4d5d16bb95ddb26d994daec61363
Raka Priyo <gugun@email.com>
2019-10-22 23:03:32 +0700

    FE - perbaikan view page collection

--- app/Model/institutional.php
+++ app/Model/institutional.php
@@ -17,4 +17,10 @@ class institutional extends Model
     {
         return self::select($field)->where('id', $parameter)->where('is_active', "Y")->first();
     }
+
+    public static function getName($id)
+    {
+        $content = content_tbl::select('institutional_id')->where('id',$id)->first()->institutional_id;
+        return self::select('institutional_name')->where('id', $content)->first()->institutional_name;
+    }
 }
--- resources/views/FE/pages/collection.blade.php
+++ resources/views/FE/pages/collection.blade.php
@@ -3,53 +3,35 @@
     active
 @endsection
 @section('content')
-<!-- Header -->
-<div class="bd-example mb-5">
-  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="{{url('bootstrap/asset-img/slider/pura-bali.jpg')}}" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-      <img src="{{url('bootstrap/asset-img/slider/pura-bali.jpg')}}" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-      <img src="{{url('bootstrap/asset-img/slider/pura-bali.jpg')}}" class="d-block w-100" alt="...">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
 <!-- Page Content -->
 <div class="container">
-    <h2 class="text-capitalize">@lang('messages.heritage_title')</h2>
+    <h2 class="text-capitalize mt-5">@lang('messages.heritage_title')</h2>
     <hr>
     <div class="row mb-5 mt-4">
         @foreach($data as $item)
         <div class="col-md-4">
-            <div class="card h-100">
-                <img class="card-img-top" src="{{$item->banner}}" alt="{{$item->banner}}" height="200" widht="400">
-                <div class="card-body">
-                    <h5 class="card-title text-uppercase"><a href="#" class="text-dark">{{$item->name}}</a></h5>
-                    <p class="card-text">
-                        <small class="card-text text-uppercase">{{\App\Model\place_tbl::placeNameLang($item->place_id)}}</small>
-                    </p>
-                    <a href="{{route('collection-detail', ['id'=>$item->id])}}" class="btn btn-block btn-{{$color_media[$item->media_type]}} text-uppercase">{{$item->media_type}}</a>
+            <a href="{{route('collection-detail', ['id'=>$item->id])}}">
+                <div class="card h-100">
+                    <img class="card-img-top" src="{{$item->banner}}" alt="{{$item->banner}}" height="200" widht="200">
+                    <div class="card-body">
+                        <h5 class="card-title text-uppercase">
+                            <a href="{{route('collection-detail', ['id'=>$item->id])}}" class="text-dark">{{$item->name}}</a>
+                        </h5>
+                        <p class="card-text">
+                            institutional : {{\App\Model\institutional::getName($item->content_id)}}<br>
+                            place : {{\App\Model\place_tbl::placeNameLang($item->place_id)}}<br>
+                            media : <span class="text text-{{$color_media[$item->media_type]}}">{{$item->media_type}}</span>
+                        </p>
+                        <p class="card-text">
+                            @php
+                                $text = App::isLocale('id') ? $item->description_ind : $item->description_en;
+                                $limit_text = strlen($text) > 250 ? substr($text, 0, 250)." ... readmore" : $text;
+                            @endphp
+                            {{$limit_text}}
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
         @endforeach
     </div>

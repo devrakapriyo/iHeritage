f984163c40d996c5d13d2c18a7ec1e51a3fe98c5
Raka Priyo <gugun@email.com>
2019-12-17 16:40:09 +0700

    BE - update rule activation account admin dan notif send email

--- app/Http/Controllers/BE/UserController.php
+++ app/Http/Controllers/BE/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Yajra\DataTables\DataTables;
 use Alert;
 

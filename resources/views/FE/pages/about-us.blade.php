3f4fabe25b64079c3dbb9ba8104fc2c23297b3b5
Raka Priyo <gugun@email.com>
2020-02-24 09:33:07 +0700

    FE - penambahan logo

--- resources/views/FE/pages/about-us.blade.php
+++ resources/views/FE/pages/about-us.blade.php
@@ -15,6 +15,51 @@
                 <img src="{{$data->banner}}" class="img-fluid" alt="{{$data->banner}}">
                 <div class="row">
                     <div class="col-md-6">
+                        <a href="https://pmli.co.id" target="_blank">
+                            <img src="{{asset('img/PMLI.png')}}" class="img" style="width:145px;height:80px;margin-left:20px;">
+                        </a>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-3">
+                        <a href="" target="_blank">
+                            {{--desktop--}}
+                            <img src="{{asset('img/AMI.jpg')}}" class="img d-none d-lg-block" style="width:100%;height:80px;">
+
+                            {{--mobile--}}
+                            <img src="{{asset('img/AMI.jpg')}}" class="img d-lg-none" style="width:50%;height:80px;">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="" target="_blank">
+                            {{--desktop--}}
+                            <img src="{{asset('img/MAKN.png')}}" class="img d-none d-lg-block" style="width:100%;height:80px;">
+
+                            {{--mobile--}}
+                            <img src="{{asset('img/MAKN.png')}}" class="img d-lg-none" style="width:50%;height:80px;">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="" target="_blank">
+                            {{--desktop--}}
+                            <img src="{{asset('img/PaSTI.jpg')}}" class="img d-none d-lg-block" style="width:100%;height:80px;">
+
+                            {{--mobile--}}
+                            <img src="{{asset('img/PaSTI.jpg')}}" class="img d-lg-none" style="width:50%;height:80px;">
+                        </a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="" target="_blank">
+                            {{--desktop--}}
+                            <img src="{{asset('img/AMI-DKI.png')}}" class="img d-none d-lg-block" style="width:100%;height:80px;">
+
+                            {{--mobile--}}
+                            <img src="{{asset('img/AMI-DKI.png')}}" class="img d-lg-none" style="width:50%;height:80px;">
+                        </a>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-6">
                         <a href="https://play.google.com/store/apps/details?id=com.pmli.iHeritage" target="_blank">
                             <img src="{{asset('img/play-store.png')}}" class="img float-right" style="width: 100%;">
                         </a>

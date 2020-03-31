9d4fd1e0b631cea0a57f660e8ed0d42b5c389e14
Raka Priyo <gugun@email.com>
2020-03-31 12:57:24 +0700

    backend - penambahan modul grafik bulan feb

--- resources/views/BE/pages/dashboard.blade.php
+++ resources/views/BE/pages/dashboard.blade.php
@@ -5,21 +5,39 @@
     @endphp
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     @if(auth('admin')->user()->institutional_id == 3)
-    <script type="text/javascript">
-        var visitor_feb = <?php echo \App\Model\visitor_counting::visitorLineChartFeb($institutional_id); ?>;
-        google.charts.load('current', {'packages':['corechart']});
-        google.charts.setOnLoadCallback(drawChart);
-        function drawChart() {
-            var data_feb = google.visualization.arrayToDataTable(visitor_feb);
-            var options_feb = {
-                title: "@lang('messages_be.dashboard_visitor_title')",
-                curveType: 'function',
-                legend: { position: 'bottom' }
-            };
-            var chart_feb = new google.visualization.LineChart(document.getElementById('linechart-feb'));
-            chart_feb.draw(data_feb, options_feb);
-        }
-    </script>
+        <script type="text/javascript">
+            var visitor_feb = <?php echo \App\Model\visitor_counting::visitorLineChartFeb($institutional_id); ?>;
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+            function drawChart() {
+                var data_feb = google.visualization.arrayToDataTable(visitor_feb);
+                var options_feb = {
+                    title: "@lang('messages_be.dashboard_visitor_title')",
+                    curveType: 'function',
+                    legend: { position: 'bottom' }
+                };
+                var chart_feb = new google.visualization.LineChart(document.getElementById('linechart-feb'));
+                chart_feb.draw(data_feb, options_feb);
+            }
+        </script>
+
+        @if(date("m") > "03")
+            <script type="text/javascript">
+                var visitor_mar = <?php echo \App\Model\visitor_counting::visitorLineChartMar($institutional_id); ?>;
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
+                function drawChart() {
+                    var data_mar = google.visualization.arrayToDataTable(visitor_mar);
+                    var options_mar = {
+                        title: "@lang('messages_be.dashboard_visitor_title')",
+                        curveType: 'function',
+                        legend: { position: 'bottom' }
+                    };
+                    var chart_mar = new google.visualization.LineChart(document.getElementById('linechart-mar'));
+                    chart_mar.draw(data_mar, options_mar);
+                }
+            </script>
+        @endif
     @endif
 
     <script type="text/javascript">
@@ -153,50 +171,97 @@
     <!-- Content Row -->
 
     @if(auth('admin')->user()->institutional_id == 3)
-    <div class="row">
+        <div class="row">
 
-        <!-- Area Chart -->
-        @if(\App\Model\visitor_counting::visitorLineChartFeb($institutional_id) != '[["Date","Visit"]]')
-        <div class="col-xl-12 col-lg-12">
-            <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">@lang('messages_be.dashboard_visitor') @lang('messages_be.dashboard_visitor_month_feb')</h6>
-                    {{--<div class="dropdown no-arrow">--}}
-                    {{--<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
-                        {{--<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>--}}
-                    {{--</a>--}}
-                    {{--<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">--}}
-                        {{--<div class="dropdown-header">Dropdown Header:</div>--}}
-                        {{--<a class="dropdown-item" href="#">Action</a>--}}
-                        {{--<a class="dropdown-item" href="#">Another action</a>--}}
-                        {{--<div class="dropdown-divider"></div>--}}
-                        {{--<a class="dropdown-item" href="#">Something else here</a>--}}
-                    {{--</div>--}}
-                    {{--</div>--}}
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <div id="linechart-feb" style="width: 100%; height: 320px;"></div>
+            <!-- Area Chart -->
+            @if(\App\Model\visitor_counting::visitorLineChartFeb($institutional_id) != '[["Date","Visit"]]')
+            <div class="col-xl-12 col-lg-12">
+                <div class="card shadow mb-4">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">@lang('messages_be.dashboard_visitor') @lang('messages_be.dashboard_visitor_month_feb')</h6>
+                        {{--<div class="dropdown no-arrow">--}}
+                        {{--<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+                            {{--<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>--}}
+                        {{--</a>--}}
+                        {{--<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">--}}
+                            {{--<div class="dropdown-header">Dropdown Header:</div>--}}
+                            {{--<a class="dropdown-item" href="#">Action</a>--}}
+                            {{--<a class="dropdown-item" href="#">Another action</a>--}}
+                            {{--<div class="dropdown-divider"></div>--}}
+                            {{--<a class="dropdown-item" href="#">Something else here</a>--}}
+                        {{--</div>--}}
+                        {{--</div>--}}
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <div id="linechart-feb" style="width: 100%; height: 320px;"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        @endif
+            @endif
 
-        <!-- Pie Chart -->
-        {{--<div class="col-xl-12 col-lg-12">--}}
-            {{--<div class="card shadow mb-4">--}}
-                {{--<!-- Card Body -->--}}
-                {{--<div class="card-body">--}}
-                    {{--<div class="table-responsive">--}}
-                        {{--<div id="donutchart" style="width: 100%; height: 373px;"></div>--}}
+            <!-- Pie Chart -->
+            {{--<div class="col-xl-12 col-lg-12">--}}
+                {{--<div class="card shadow mb-4">--}}
+                    {{--<!-- Card Body -->--}}
+                    {{--<div class="card-body">--}}
+                        {{--<div class="table-responsive">--}}
+                            {{--<div id="donutchart" style="width: 100%; height: 373px;"></div>--}}
+                        {{--</div>--}}
                     {{--</div>--}}
                 {{--</div>--}}
             {{--</div>--}}
-        {{--</div>--}}
-    </div>
+        </div>
+
+        @if(date("m") > "03")
+            <div class="row">
+
+                <!-- Area Chart -->
+                @if(\App\Model\visitor_counting::visitorLineChartMar($institutional_id) != '[["Date","Visit"]]')
+                    <div class="col-xl-12 col-lg-12">
+                        <div class="card shadow mb-4">
+                            <!-- Card Header - Dropdown -->
+                            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                <h6 class="m-0 font-weight-bold text-primary">@lang('messages_be.dashboard_visitor') @lang('messages_be.dashboard_visitor_month_mar')</h6>
+                                {{--<div class="dropdown no-arrow">--}}
+                                {{--<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+                                {{--<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>--}}
+                                {{--</a>--}}
+                                {{--<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">--}}
+                                {{--<div class="dropdown-header">Dropdown Header:</div>--}}
+                                {{--<a class="dropdown-item" href="#">Action</a>--}}
+                                {{--<a class="dropdown-item" href="#">Another action</a>--}}
+                                {{--<div class="dropdown-divider"></div>--}}
+                                {{--<a class="dropdown-item" href="#">Something else here</a>--}}
+                                {{--</div>--}}
+                                {{--</div>--}}
+                            </div>
+                            <!-- Card Body -->
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <div id="linechart-mar" style="width: 100%; height: 320px;"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+            <!-- Pie Chart -->
+                {{--<div class="col-xl-12 col-lg-12">--}}
+                {{--<div class="card shadow mb-4">--}}
+                {{--<!-- Card Body -->--}}
+                {{--<div class="card-body">--}}
+                {{--<div class="table-responsive">--}}
+                {{--<div id="donutchart" style="width: 100%; height: 373px;"></div>--}}
+                {{--</div>--}}
+                {{--</div>--}}
+                {{--</div>--}}
+                {{--</div>--}}
+            </div>
+        @endif
     @endif
 
     <div class="row">
@@ -207,7 +272,7 @@
             <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">@lang('messages_be.dashboard_visitor')  @lang('messages_be.dashboard_visitor_month_mar')</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">@lang('messages_be.dashboard_visitor')</h6>
                     {{--<div class="dropdown no-arrow">--}}
                     {{--<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
                         {{--<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>--}}
